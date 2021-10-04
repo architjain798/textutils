c51e9434c341672dbdf9272f933b51890b0c3a17
Archit Jain <archit.jain@betsol.com>
2021-10-04 17:47:44 +0530

    linked

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
-
-import './App.css';
-import NavBar from './Components/NavBar';
-import { TextForm } from './Components/TextForm';
+import "./App.css";
+import NavBar from "./Components/NavBar";
+import { TextForm } from "./Components/TextForm";
 
 function App() {
   return (
     <>
       <NavBar title="TextUtils" aboutText="About Us" />
       <div className="container">
-        <TextForm heading="Enter Text to Anaylze" />
+        <TextForm heading="Enter Text to Anaylze g" />
       </div>
     </>
   );
 }
-
 
 export default App;
